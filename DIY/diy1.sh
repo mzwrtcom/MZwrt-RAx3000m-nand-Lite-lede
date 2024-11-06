4f5e6347597c20cc61e3019e68f86696a494bc27
mzal <58111339+mzwrt@users.noreply.github.com>
2024-11-06 10:27:57 +0800

    Create diy1.sh

---
+++ DIY/diy1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# MZwrt script
+#================================================= 
+
+
+##添加自己的插件库
+echo -e "\nsrc-git mzwrt_ipk_lede https://github.com/mzwrt/mzwrt_ipk_lede" >> feeds.conf.default
