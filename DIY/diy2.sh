f0de00e659c6fb6066b2a6cf17f8f9d4af6ecf8a
mzal <58111339+mzwrt@users.noreply.github.com>
2024-11-06 10:52:18 +0800

    Update diy2.sh

--- DIY/diy2.sh
+++ DIY/diy2.sh
@@ -9,6 +9,9 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 ##更改主机名
 sed -i "s/hostname='.*'/hostname='MzWrt'/g" package/base-files/files/bin/config_generate
 
+#删除默认后台密码
+sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
 ##加入作者信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='MzWrt'/g"  package/lean/default-settings/files/zzz-default-settings
 echo -e "[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[0m
