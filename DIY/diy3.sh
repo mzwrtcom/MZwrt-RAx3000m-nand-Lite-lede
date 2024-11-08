887ceab1a2a860ccc79d2c8d0d41178b1390f9a6
mzal <58111339+mzwrt@users.noreply.github.com>
2024-11-08 13:53:53 +0800

    Update diy3.sh

--- DIY/diy3.sh
+++ DIY/diy3.sh
@@ -18,6 +18,9 @@ cat <<EOL >> package/base-files/files/etc/uci-defaults/99-MzWrt-settings
 # 设置主机名
 uci set system.@system[0].hostname='MzWrt'
 
+# 修改默认IP
+uci set network.lan.ipaddr='192.168.10.1'
+
 # 设置 2.4GHz 频段的国家为中国
 uci set wireless.radio0.country='CN'
 # 设置 5GHz 频段的国家为中国
@@ -30,8 +33,8 @@ uci set wireless.radio1.channel='36'  # 设置 5GHz 信道为 36
 # 设置 2.4GHz 频段的无线模式（支持 802.11n，20MHz 或 40MHz，最大带宽 40MHz）
 uci set wireless.radio0.htmode='HT40'  # 设置 2.4GHz 频段为 40MHz 带宽
 
-# 设置 5GHz 频段的无线模式为 VHT160（最大带宽 160MHz）
-uci set wireless.radio1.htmode='VHT160'  # 设置 5GHz 带宽为 160MHz
+# 设置 5GHz 频段的无线模式为 HE160（最大带宽 160MHz）
+uci set wireless.radio1.htmode='HE160'  # 设置 5GHz 带宽为 160MHz
 
 # 开启 Beamforming 功能
 uci set wireless.radio0.beamforming='1'  # 开启 2.4GHz 的 Beamforming
@@ -53,7 +56,10 @@ uci set wireless.default_radio1.ssid='MzWrt-5G'
 uci commit wireless
 
 # 重启网络
-/etc/init.d/network restart
+# /etc/init.d/network restart
+
+# 提交修改的默认IP
+uci commit network
 
 # 如果您确实需要重启系统或修改主机名，也可以执行
 # 提交主机名配置
