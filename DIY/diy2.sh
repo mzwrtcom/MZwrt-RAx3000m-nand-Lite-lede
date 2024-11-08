37417b8a801e9e57b3a99bc806467799bba4fb37
mzal <58111339+mzwrt@users.noreply.github.com>
2024-11-08 13:55:28 +0800

    Update diy2.sh

--- DIY/diy2.sh
+++ DIY/diy2.sh
@@ -23,19 +23,3 @@ echo -e "[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[0m
 [33m\     /_/                                    /[0m
 [33m\  M Z W r t  By  https://github.com/mzwrt   /[0m
 [31m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[0m" > package/base-files/files/etc/banner
-
-
-##WiFi
-sed -i "s/OpenWrt/MzWrt-2.4G/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.2G.dat
-sed -i "s/OpenWrt/MzWrt-5G/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.5G.dat
-# 配置wifi密码
-sed -i "s/AuthMode=OPEN/AuthMode=WPA2PSK/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.2G.dat
-sed -i "s/AuthMode=OPEN/AuthMode=WPA2PSK/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.5G.dat
-sed -i "s/WPAPSK1=12345678/WPAPSK1=admin123/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.2G.dat
-sed -i "s/WPAPSK1=12345678/WPAPSK1=admin123/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.5G.dat
-#配置国家代码为CN
-sed -i "s/CountryCode=US/CountryCode=CN/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.2G.dat
-sed -i "s/CountryCode=US/CountryCode=CN/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.5G.dat
-#配置信道
-sed -i "s/Channel=6/Channel=13/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.2G.dat
-sed -i "s/Channel=36/Channel=56/g" package/lean/mt/drivers/mt_wifi/files/mt7915.1.5G.dat
